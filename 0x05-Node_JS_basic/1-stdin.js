8326fa813b6fe1d597078f8a65b73b01b95d9b96
Alayande-442 <alayandesteven@gmail.com>
2025-01-17 15:27:46 +0000

    test

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,16 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Display the welcome message
-console.log('Welcome to ALX, what is your name?');
-
-// Set up stdin to accept user input
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim(); // Get the input and remove extra whitespace
-
-  // Display the entered name
-  console.log(`Your name is: ${name}`);
-
-  // Display the closing message when user input is received
-  console.log('This important software is now closing');
-
-  // Exit the program
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
